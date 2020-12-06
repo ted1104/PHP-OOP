9122800a55cfb87bab04bed303d367f72bb2039a
Teddy Walter <teddywalter2016@gmail.com>
2020-12-06 18:53:25 +0000

    Les constantes

---
+++ 28constant.php
@@ -0,0 +1,19 @@
+<?php
+//__LINE__
+//__FILE__
+//__DIR__
+//__FUNCTION__
+//__CLASS__
+//__METHOD__
+//__NAMESPACE__
+//__TRAIT__
+    
+    echo "line Number :".__LINE__."<br>";
+    echo "The full path of this file is :".__FILE__."<br>";   echo "The full path of this directory :".__DIR__."<br>";
+    
+    function Teddy(){
+        echo "The function name is :".__FUNCTION__."<br>";
+    }
+    Teddy();
+
+?>
